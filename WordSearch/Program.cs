7adb9f09c2dbd558ed7aee6c479072c76a28dbc3
madkar85 <madeleine.karlsson@outlook.com>
2021-10-22 16:00:03 +0200

    Changes Sort method

--- WordSearch/Program.cs
+++ WordSearch/Program.cs
@@ -14,6 +14,7 @@ namespace WordSearch
             string[] docs = { @"C:\Users\madel\OneDrive\Skrivbord\documents\Doc1.txt",
                 @"C:\Users\madel\OneDrive\Skrivbord\documents\Doc2.txt",
                 @"C:\Users\madel\OneDrive\Skrivbord\documents\Doc3.txt" };
+
             var allDocs = new List<string[]>();
 
             for (int i = 0; i <= docs.Length - 1; i++)
@@ -26,17 +27,16 @@ namespace WordSearch
 
         public static string[] ReadDocument(string docName)
         {
-            var words = File.ReadAllText(docName)
-            .Split(new[] { ' ', ',', '[', '\n', ';', ':', '.', '!', '?', '-', ']', '+' },
-            StringSplitOptions.RemoveEmptyEntries);
+            var file = File.ReadAllText(docName);
 
-            return words;
+            return Regex.Split(file, @"[^\w]+").Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
         }
 
         public static void Menu(List<string[]> allDocs)
         {
             bool keepgoing = true;
             Console.WriteLine("Hej!");
+
             while (keepgoing)
             {
                 Console.WriteLine("Vad vill du göra med dokumenten?");
@@ -46,7 +46,7 @@ namespace WordSearch
                 Console.WriteLine("4. Skriv ut ett antal ord ur sorterat dokument");
                 Console.WriteLine("5. Avsluta");
 
-               string answer = Console.ReadLine();
+                string answer = Console.ReadLine();
 
                 switch (answer)
                 {
@@ -57,10 +57,14 @@ namespace WordSearch
                         //PrintResult();
                         break;
                     case "3":
-                        SortDocument(allDocs);
+                       allDocs = SortDocument(allDocs);
                         break;
                     case "4":
-                       //PrintXAmountOfWords
+                        PrintXAmountOfWords(allDocs[0], 15);
+                        Console.WriteLine();
+                        PrintXAmountOfWords(allDocs[1], 15);
+                        Console.WriteLine();
+                        PrintXAmountOfWords(allDocs[2], 15);
                         break;
                     case "5":
                         keepgoing = false;
@@ -72,19 +76,33 @@ namespace WordSearch
             }
         }
 
+        public static void PrintXAmountOfWords(string[] words, int count)
+        {
+            if (count == 0 || words.Length < 1) return;
+
+            var word = words[0];
+
+            Console.WriteLine(word);
+
+            PrintXAmountOfWords(words.Skip(1).ToArray(), count - 1);
+
+            return;
+        }
+
         public static void ErrorMessage()
         {
             Console.WriteLine("Fel input, vänligen försök igen.");
             Console.WriteLine();
         }
 
+        public static string[] SortDocumentWords(string[] document)
+        {
+            return document.OrderBy(word => word).ToArray();
+        }
+
         public static List<string[]> SortDocument(List<string[]> allDocs)
         {
-            allDocs = allDocs.
-                OrderBy(allDocs => allDocs[0]).
-                ThenBy(allDocs => allDocs[1]).
-                ToList();
-            return allDocs;
+            return allDocs.Select(x => SortDocumentWords(x)).ToList();
         }
 
     }
